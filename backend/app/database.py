2e815931a84fe1c80c997d961b4833c59f36d733
daehan <gangdehan@gmail.com>
2025-06-08 20:10:11 +0900

    path upd

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("app/agaricleanerdb-firebase-adminsdk-fbsvc-b7483c5bb1.json")
+cred = credentials.Certificate("agaricleanerdb-firebase-adminsdk-fbsvc-b7483c5bb1.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
