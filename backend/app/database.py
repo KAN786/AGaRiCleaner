d004aa2787edc6a8aef259b82d574d8c996fa2bd
m1ntree <minh811@cau.ac.kr>
2025-06-08 20:15:43 +0900

    refactor(firebase): read Firebase service account from FIREBASE_ADMIN_KEY env var to support CI/CD deployment on Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,22 @@
+# import firebase_admin
+# from firebase_admin import credentials, firestore
+
+# cred = credentials.Certificate("app/agaricleanerdb-firebase-adminsdk-fbsvc-b7483c5bb1.json")
+# firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
+# do the above for local
+
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+firebase_key_dict = json.loads(os.environ["FIREBASE_ADMIN_KEY"])
+cred = credentials.Certificate(firebase_key_dict)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
